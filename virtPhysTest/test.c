c53c87ba72dd0c05c75a728403a7976f9f3b94f7
Christopher Feener <christopher.feener@wsu.edu>
2017-06-27 14:43:20 -0700

    Added files to test in qemu. Contain virt_to_phys().

---
+++ virtPhysTest/test.c
@@ -0,0 +1,33 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/printk.h>
+#include <linux/module.h>
+#include <linux/gfp.h>
+#include <linux/types.h>
+#include <linux/mm.h>
+#include <linux/mm_types.h>
+#include <linux/slab.h>
+
+static int vm_mem_test_init(void)
+{
+
+    void *km;
+
+    printk(KERN_ALERT "vm mem test init\n");
+
+    km = kmalloc(1,GFP_KERNEL);
+
+    if(km == NULL) {
+        printk(KERN_ALERT "Could not kmalloc\n");
+        return -1;
+    } else {
+        printk(KERN_ALERT "Allocated\n");
+        printk(KERN_ALERT "Virtual addr : %x\n",(unsigned int)km);
+        printk(KERN_ALERT "Kernel physical addr : 0x%x\n",virt_to_phys(km));
+        kfree(km);
+    }
+
+  return 0;
+
+}
+
