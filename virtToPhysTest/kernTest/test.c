e58277763a461d20395702bdc19bce94abc41e81
Christopher Feener <christopher.feener@wsu.edu>
2017-06-28 11:41:31 -0700

    Added some test text.

--- virtToPhysTest/kernTest/test.c
+++ virtToPhysTest/kernTest/test.c
@@ -11,7 +11,64 @@
 #include <linux/fs.h>	//For filp
 #include <asm/uaccess.h>	//For segment descriptors.
 
-static int vm_mem_test_init(void)
+
+static int virtToPhys(void)
+{
+	int fd;
+	//From http://www.linuxjournal.com/node/8110/print
+	fd = sys_open("/proc/9643/maps", O_RDONLY, 0);
+	if (fd >=0) {
+		
+
+		sys_close(fd);
+	}
+
+	return 0;
+}
+
+module_init(virtToPhys);
+module_exit(virtToPhys);
+/*
+int init_module(void) //From https://www.howtoforge.com/reading-files-from-the-linux-kernel-space-module-driver-fedora-14
+{
+    // Create variables
+    struct file *f;
+    char buf[128];
+    mm_segment_t fs;
+    int i;
+    // Init the buffer with 0
+    for(i=0;i<128;i++)
+        buf[i] = 0;
+    // To see in /var/log/messages that the module is operating
+    printk(KERN_INFO "My module is loaded\n");
+    // I am using Fedora and for the test I have chosen following file
+    // Obviously it is much smaller than the 128 bytes, but hell with it =)
+    f = filp_open("/proc/9643/maps", O_RDONLY, 0);	//XXX get_fs may be for something else.
+    if(f == NULL)
+        printk(KERN_ALERT "filp_open error!!.\n");
+    else{
+        // Get current segment descriptor
+        fs = get_fs();
+        // Set segment descriptor associated to kernel space
+        set_fs(get_ds());
+        // Read the file
+        f->f_op->read(f, buf, 128, &f->f_pos);
+        // Restore segment descriptor
+        set_fs(fs);
+        // See what we read from file
+        printk(KERN_INFO "buf:%s\n",buf);
+    }
+    filp_close(f,NULL);
+    return 0;
+}
+
+void cleanup_module(void)
+{
+    printk(KERN_INFO "My module is unloaded\n");
+}
+*/
+/*
+static int vm_mem_test_init(void)	//From https://www.spinics.net/lists/newbies/msg53705.html
 {
 
     void *km;
@@ -26,7 +83,7 @@ static int vm_mem_test_init(void)
     } else {
         printk(KERN_ALERT "Allocated\n");
         printk(KERN_ALERT "Virtual addr : %x\n",(unsigned int)km);
-        printk(KERN_ALERT "Kernel physical addr : 0x%x\n",virt_to_phys(km));
+        printk(KERN_ALERT "Kernel physical addr : 0x%x\n",virt_to_phys(km));	//Here is the core concept!
         kfree(km);
     }
 
@@ -36,3 +93,4 @@ static int vm_mem_test_init(void)
 
 module_init(vm_mem_test_init);
 module_exit(vm_mem_test_init);
+*/
