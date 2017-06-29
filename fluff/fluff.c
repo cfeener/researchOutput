733bb6175ff47124abeb007063451f25b670b7a6
Christopher Feener <christopher.feener@wsu.edu>
2017-06-29 15:11:44 -0700

    Tested physical address and page descriptor inside fluff.c.

--- fluff/fluff.c
+++ fluff/fluff.c
@@ -5,15 +5,19 @@
 
 #include <linux/rmap.h>	//For anon... functions.
 
-static int physToPID(void)
+int init_module(void)
 {
 	struct page *p = kmalloc(sizeof(struct page), GFP_KERNEL);	//Not sure if kmalloc is necessary.
 
-	int p_index;
-	p_index = p / PAGE_SIZE;	//PAGE_SIZE may be 4096, or 4k.
+	long long int p_index;
+	p_index = (long long int)p / PAGE_SIZE;	//PAGE_SIZE may be 4096, or 4k.
+	printk(KERN_ALERT "p = %p\n, PAGE_SIZE = %lu\np_index = %llx\n", p, PAGE_SIZE, p_index);
 
-	int test = p->_mapcount;	//p->_mapcount works!
+//	int test = p->_mapcount;	//p->_mapcount works!
+
+	return 0;
 }
 
-module_init(physToPID);
-module_exit(physToPID);
+void cleanup_module(void){
+	printk(KERN_ALERT "Cleaning up.\n");
+}
