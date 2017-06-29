9ea3dcfa7b107972f0c587cba0926eeab2c42b86
Christopher Feener <christopher.feener@wsu.edu>
2017-06-29 15:31:33 -0700

    Attempted to fix "p->_mapcount".

--- fluff/fluff.c
+++ fluff/fluff.c
@@ -11,9 +11,10 @@ int init_module(void)
 
 	long long int p_index;
 	p_index = (long long int)p / PAGE_SIZE;	//PAGE_SIZE may be 4096, or 4k.
-	printk(KERN_ALERT "p = %p\n, PAGE_SIZE = %lu\np_index = %llx\n", p, PAGE_SIZE, p_index);
+	printk(KERN_ALERT "p = %p\nPAGE_SIZE = %lu\np_index = %llx\n", p, PAGE_SIZE, p_index);
 
-//	int test = p->_mapcount;	//p->_mapcount works!
+	if ( p->_mapcount > 0)
+		printk(KERN_ALERT "YES\n");	//p->_mapcount works!
 
 	return 0;
 }
