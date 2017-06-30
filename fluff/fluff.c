aed5299f907bf4cc11aff47b21cde906dd6c233d
Christopher Feener <christopher.feener@wsu.edu>
2017-06-30 13:13:24 -0700

    Was starting from page descriptor whole time. Practice on phys addr.

--- fluff/fluff.c
+++ fluff/fluff.c
@@ -11,15 +11,22 @@
 
 int init_module(void)
 {
+	//Find physical address. Or simply start with page descriptor:
 	struct page *p = kmalloc(sizeof(struct page), GFP_KERNEL);	//Not sure if kmalloc is necessary.
 
-	void * anon_vma;
-	long long int p_m;
-	long long int p_index;
-	p_index = (long long int)p / PAGE_SIZE;	//PAGE_SIZE may be 4096, or 4k.
-	printk(KERN_ALERT "p = %p\nPAGE_SIZE = %lu\np_index = %llx\n", p, PAGE_SIZE, p_index);
+	void * fileOrProc;
+	long long int LSB;
+	long long int pmapping;
+//	long long int p_index_addr;
+//	void * p_index;
 
-	printk(KERN_ALERT "Atomic p->_mapcount: %d\n", atomic_read(&p->_mapcount));
+	//Find page descriptor.
+//	p_index_addr = (long long int)p / PAGE_SIZE;	//PAGE_SIZE may be 4096, or 4k.
+//	p_index = (void *)p_index_addr;
+	
+	printk(KERN_ALERT "p = %p\n", p);
+
+	printk(KERN_ALERT "p->_mapcount: %d\n", atomic_read(&p->_mapcount));
 
 	if ( atomic_read(&p->_mapcount) < 0 ) {
 		printk(KERN_ALERT "Error: _mapcount too small.\n");
@@ -27,10 +34,11 @@ int init_module(void)
 	}
 	//Read *mapping:
 	printk(KERN_ALERT "p->mapping = %p\n", p->mapping);
-	p_m = ((unsigned long)p->mapping) >> 1;	//mask low bit
-	printk(KERN_ALERT "masked out lower bit = %llx\n", p_m);
+	LSB = ((unsigned long)p->mapping) & 1;
+	pmapping = ((unsigned long)p->mapping) >> 1;	//mask low bit
+	printk(KERN_ALERT "Address = %llx, lowest bit = %llx", pmapping, LSB);
 
-	anon_vma = (void *)p_m;	//Set address.
+	fileOrProc = (void *)pmapping;	//Set address.
 
 	return 0;
 }
