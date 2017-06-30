7ad7baf9f55a0261361d456c3265d2c03bb54744
Christopher Feener <christopher.feener@wsu.edu>
2017-06-30 11:26:41 -0700

    Added a script to do everything, and read mapping in fluff.c

--- fluff/fluff.c
+++ fluff/fluff.c
@@ -9,7 +9,7 @@ int init_module(void)
 {
 	struct page *p = kmalloc(sizeof(struct page), GFP_KERNEL);	//Not sure if kmalloc is necessary.
 
-	int p_m;
+	long long int p_m;
 	long long int p_index;
 	p_index = (long long int)p / PAGE_SIZE;	//PAGE_SIZE may be 4096, or 4k.
 	printk(KERN_ALERT "p = %p\nPAGE_SIZE = %lu\np_index = %llx\n", p, PAGE_SIZE, p_index);
@@ -22,7 +22,8 @@ int init_module(void)
 	}
 	//Read *mapping:
 	printk(KERN_ALERT "p->mapping = %p\n", p->mapping);
-	p_m = ((unsigned long)p->mapping) /*>> 1*/;	//mask low bit
+	p_m = ((unsigned long)p->mapping) >> 1;	//mask low bit
+	printk(KERN_ALERT "masked out lower bit = %llx\n", p_m);
 
 	return 0;
 }
