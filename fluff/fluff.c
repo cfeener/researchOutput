ea7a268ae691ae9c30baa76f93a50b61d5fd7a61
Christopher Feener <christopher.feener@wsu.edu>
2017-06-30 11:02:15 -0700

    Started using *mapping address in fluff.c.

--- fluff/fluff.c
+++ fluff/fluff.c
@@ -9,11 +9,20 @@ int init_module(void)
 {
 	struct page *p = kmalloc(sizeof(struct page), GFP_KERNEL);	//Not sure if kmalloc is necessary.
 
+	int p_m;
 	long long int p_index;
 	p_index = (long long int)p / PAGE_SIZE;	//PAGE_SIZE may be 4096, or 4k.
 	printk(KERN_ALERT "p = %p\nPAGE_SIZE = %lu\np_index = %llx\n", p, PAGE_SIZE, p_index);
 
-	printk(KERN_ALERT "Atomic: %d\n", atomic_read(&p->_mapcount));
+	printk(KERN_ALERT "Atomic p->_mapcount: %d\n", atomic_read(&p->_mapcount));
+
+	if ( atomic_read(&p->_mapcount) < 0 ) {
+		printk(KERN_ALERT "Error: _mapcount too small.\n");
+		return 1;
+	}
+	//Read *mapping:
+	printk(KERN_ALERT "p->mapping = %p\n", p->mapping);
+	p_m = ((unsigned long)p->mapping) /*>> 1*/;	//mask low bit
 
 	return 0;
 }
