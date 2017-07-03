5233a8f3568f37e44bbb8650ca67b591c06c1eda
Christopher Feener <christopher.feener@wsu.edu>
2017-07-03 14:14:46 -0700

    Tested kmalloc in fluff.c. Is necessary for page allocation.

--- fluff/fluff.c
+++ fluff/fluff.c
@@ -12,12 +12,12 @@
 
 int init_module(void)
 {
-	//Find physical address. (skipped)
+	//TODO Find physical address, then divide by PAGE_SIZE for page desc. (Failed; skipped)
 	//Or simply start with page descriptor:
-	struct page *p = kmalloc(sizeof(struct page), GFP_KERNEL);	//Not sure if kmalloc is necessary.
+	struct page *p = kmalloc(sizeof(struct page), GFP_KERNEL);	//kmalloc is necessary.
 	phys_addr_t ptr = virt_to_phys(p);
 
-	void * fileOrProc;
+	struct anon_vma * VMA;
 	long long int LSB;
 	long long int pmapping;
 //	long long int p_addr;
@@ -39,9 +39,14 @@ int init_module(void)
 	printk(KERN_ALERT "p->mapping = %p\n", p->mapping);
 	LSB = ((unsigned long)p->mapping) & 1;
 	pmapping = ((unsigned long)p->mapping) >> 1;	//mask low bit
-	printk(KERN_ALERT "Address = %llx, lowest bit = %llx", pmapping, LSB);
+	if ( LSB == 1 ) {
+		printk(KERN_ALERT "VMA");
+	} else {
+		printk(KERN_ALERT "File (or device)");
+	}
+	printk(KERN_ALERT " address = %llx, lowest bit = %llx", pmapping, LSB);
 
-	fileOrProc = (void *)pmapping;	//Set address.
+	VMA = (struct anon_vma *)pmapping;	//Set address.
 
 	return 0;
 }
