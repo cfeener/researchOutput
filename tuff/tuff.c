bb19aacdab0c14c057a61f32e5d8bb5f9a128d2a
Christopher Feener <christopher.feener@wsu.edu>
2017-07-13 13:46:29 -0700

    Finished testing iterator for task_struct current.

--- tuff/tuff.c
+++ tuff/tuff.c
@@ -1,5 +1,6 @@
 /* Christopher Feener
  * Tuff.c
+ * Finished testing iterator for struct task_struct current.
  * Tests current->tsk_struct's->vma. Will focus on page_address_in_vma() from rmap later.
  */
 
@@ -10,11 +11,22 @@
 
 #include <linux/rmap.h>	//For page_address_in_vma().
 #include <linux/sched.h>	//For task_struct's.
+#include <linux/list.h>	//For list_for_each.
 
 int init_module(void)
 {
-	struct task_struct *temp_curr;
-	temp_curr = current;
+	struct list_head *list;
+	struct task_struct *task;
+	task = current;
+
+	printk(KERN_ALERT "Name: %s, PID: %d", task->comm, task->pid);
+
+//	while (task->pid != 0) {
+	list_for_each(list, &current->children) {
+		task = list_entry(list, struct task_struct, sibling);
+//		task = task->parent; 	//Try child, next, and finally actual iterator.
+		printk(KERN_ALERT "Name: %s, PID: %d", task->comm, task->pid);
+	}
 	//->mm->mmap->anon_vma;
 
 	return 0;
